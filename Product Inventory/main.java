814472288aaf1952072cf485ffa8e766987aa10b
Aarav D.C <dc.aarav@gmail.com>
2025-02-09 17:36:35 +0545

    Product class

---
+++ Product Inventory/main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class product{
+    private String id;
+    private double price;
+    private int quantity;
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+}
